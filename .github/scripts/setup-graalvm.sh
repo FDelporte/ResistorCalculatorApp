3f98dc9dc3e0b303dc54c7f0989e6a07aaea1cba
Frank Delporte <f.delporte@televic.com>
2020-10-29 22:11:51 +0100

    Working on GitHub action

---
+++ .github/scripts/setup-graalvm.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+set -e
+echo "Downloading"
+sh -c "curl -sL https://github.com/graalvm/graalvm-ce-builds/releases/download/vm-20.2.0/graalvm-ce-java11-linux-amd64-20.2.0.tar.gz --output graalvm.tar.gz"
+
+echo "Extracting"
+sh -c "mkdir graalvm"
+sh -c "tar -xf graalvm.tar.gz -C graalvm --strip-components=1"
+
+echo "Exporting environment variables."
+sh -c "export GRAALVM_HOME=graalvm"
+sh -c "export JAVA_HOME=$GRAALVM_HOME"
+sh -c "export PATH=$PATH:$GRAALVM_HOME/bin"
+
+echo "GRAALVM_HOME=$GRAALVM_HOME"
+echo "JAVA_HOME=$JAVA_HOME"
+echo "PATH=$PATH"
