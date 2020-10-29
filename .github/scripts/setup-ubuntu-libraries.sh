887943562e4d56209091a664e371af4a082e23d4
Frank Delporte <f.delporte@televic.com>
2020-10-29 23:15:19 +0100

    libraries

---
+++ .github/scripts/setup-ubuntu-libraries.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+sh -c "apt install libasound2-dev"
+sh -c "apt install libavcodec-dev"
+sh -c "apt install libavformat-dev"
+sh -c "apt install libavutil-dev"
+sh -c "apt install libgl-dev"
+sh -c "apt install libgtk-3-dev"
+sh -c "apt install libpango1.0-dev"
+sh -c "apt install libxtst-dev"
