992a4dd43ac704b3ebf6a0c8678be7f5b3b2750a
Frank Delporte <f.delporte@televic.com>
2020-11-04 19:43:04 +0100

    First attempt windows actions scripts

---
+++ .github/scripts/setup-windows-graalvm.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+set -e
+echo "Downloading"
+sh -c "curl -sL https://github.com/graalvm/graalvm-ce-builds/releases/download/vm-20.2.0/graalvm-ce-java11-windows-amd64-20.2.0.zip --output graalvm.tar.gz"
+
+echo "Extracting"
+sh -c "mkdir graalvm"
+sh -c "tar -xf graalvm.tar.gz -C graalvm --strip-components=1"
+
+echo "Exporting environment variables."
+sh -c "export GRAALVM_HOME=graalvm"
+sh -c "export JAVA_HOME=$GRAALVM_HOME"
+sh -c "export PATH=$PATH:$GRAALVM_HOME/bin"
+
+echo "GRAALVM_HOME=$GRAALVM_HOME"
+echo "JAVA_HOME=$JAVA_HOME"
+echo "PATH=$PATH"
+
+echo "--------------------------------------------------"
+echo "JAVA version"
+sh -c "java -version"
+
+echo "--------------------------------------------------"
+echo "MAVEN version"
+sh -c "mvn -version"
