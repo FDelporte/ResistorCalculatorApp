9b970818b3b6c965135e6a477cdbb823ae04ff6b
Frank Delporte <frank@webtechie.be>
2020-11-04 20:23:17 +0100

    export graalvm path

--- .github/scripts/setup-windows-graalvm.sh
+++ .github/scripts/setup-windows-graalvm.sh
@@ -4,12 +4,11 @@ echo "Downloading"
 sh -c "curl -sL https://github.com/graalvm/graalvm-ce-builds/releases/download/vm-20.2.0/graalvm-ce-java11-windows-amd64-20.2.0.zip --output graalvm.zip"
 
 echo "Extracting"
-sh -c "mkdir C:\graalvm"
-#sh -c "jar xf test.zip"
-sh -c "unzip graalvm.zip -d C:\graalvm"
+sh -c "mkdir C:/graalvm"
+sh -c "unzip graalvm.zip -d C:/graalvm"
 
 echo "Exporting environment variables."
-sh -c "export GRAALVM_HOME=graalvm"
+sh -c "export GRAALVM_HOME=C:graalvm/graalvm-ce-java11-20.2.0/"
 sh -c "export JAVA_HOME=$GRAALVM_HOME"
 sh -c "export PATH=$PATH:$GRAALVM_HOME/bin"
 
