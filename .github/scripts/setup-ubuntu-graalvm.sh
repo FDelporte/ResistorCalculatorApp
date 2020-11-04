ef2fa778cb1156cfba4414cf876c58961587957e
Frank Delporte <f.delporte@televic.com>
2020-11-04 22:45:48 +0100

    setting graalvm_home for linux

--- .github/scripts/setup-ubuntu-graalvm.sh
+++ .github/scripts/setup-ubuntu-graalvm.sh
@@ -1,25 +1,31 @@
 #!/bin/sh
 set -e
-echo "Downloading"
+echo "--- Downloading --- "
 sh -c "curl -sL https://github.com/graalvm/graalvm-ce-builds/releases/download/vm-20.2.0/graalvm-ce-java11-linux-amd64-20.2.0.tar.gz --output graalvm.tar.gz"
 
-echo "Extracting"
-sh -c "mkdir graalvm"
-sh -c "tar -xf graalvm.tar.gz -C graalvm --strip-components=1"
+echo "--- Extracting ---"
+sh -c "mkdir -p ~/graalvm"
+sh -c "tar -xf graalvm.tar.gz -C ~/graalvm --strip-components=1"
+sh -c "rm graalvm.tar.gz"
 
-echo "Exporting environment variables."
-sh -c "export GRAALVM_HOME=graalvm"
-sh -c "export JAVA_HOME=$GRAALVM_HOME"
-sh -c "export PATH=$PATH:$GRAALVM_HOME/bin"
+echo "--- Setting environment variables ---"
+export GRAALVM_HOME=~/graalvm
+export JAVA_HOME=~/graalvm
+export PATH=~/graalvm/bin:$PATH
 
+echo "--- Checking environment variables ---"
 echo "GRAALVM_HOME=$GRAALVM_HOME"
 echo "JAVA_HOME=$JAVA_HOME"
 echo "PATH=$PATH"
 
-echo "--------------------------------------------------"
+echo "--- GRAALVM ---"
+echo "GRAALVM version"
+sh -c "~/graalvm/bin/java -version"
+
+echo "--- JAVA ---"
 echo "JAVA version"
 sh -c "java -version"
 
-echo "--------------------------------------------------"
+echo "--- MAVEN ---"
 echo "MAVEN version"
 sh -c "mvn -version"
