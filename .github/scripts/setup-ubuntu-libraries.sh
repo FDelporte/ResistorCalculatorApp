c63b764d67c85c357c9fadb8b8d7e664b26efde0
Frank Delporte <f.delporte@televic.com>
2020-11-04 23:27:35 +0100

    added android build

--- .github/scripts/setup-ubuntu-libraries.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-sh -c "sudo apt install libasound2-dev"
-sh -c "sudo apt install libavcodec-dev"
-sh -c "sudo apt install libavformat-dev"
-sh -c "sudo apt install libavutil-dev"
-sh -c "sudo apt install libgl-dev"
-sh -c "sudo apt install libgtk-3-dev"
-sh -c "sudo apt install libpango1.0-dev"
-sh -c "sudo apt install libxtst-dev"
